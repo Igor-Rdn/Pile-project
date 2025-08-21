a756d0d0fedb1719781ba4cb8432f094d4d3fac3
Igor <igor_rdn@mail.ru>
2025-08-21 16:24:52 +0300

    Knight d.2

--- cmd/Knight_move/main.go
+++ cmd/Knight_move/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 type position struct {
-	x int // i
-	y int // j
+	x int //a -> h
+	y int //1 -> 8
 }
+
+/* type testCases struct {
+	start position
+	end   position
+} */
 
 // Проверка валидности позиции
 func (p position) isValid() bool {
@@ -30,14 +36,13 @@ func (curPos position) knightMove() (res []position) {
 	return
 }
 
+// Поиск кратчайшего пути
 func findShortestPath(startPos, endPos position) []position {
 
 	queue := []position{startPos}
 	parent := make(map[position]position) //[Child]Parent
-	//i := 0
 
 	for len(queue) > 0 {
-		//i++
 
 		curPos := queue[0]
 
@@ -50,23 +55,21 @@ func findShortestPath(startPos, endPos position) []position {
 				queue = append(queue, availablePos)
 				parent[availablePos] = curPos
 			}
-
 		}
-
 		queue = queue[1:]
-		//fmt.Println("i", i, queue)
-
 	}
-	return reconstructPath(endPos, parent)
+	return recoveryPath(endPos, parent)
 }
 
-func reconstructPath(parentPos position, parent map[position]position) (resSlice []position) {
+// Восстановление итогового пути
+func recoveryPath(parentPos position, parent map[position]position) (path []position) {
 
-	resSlice = append(resSlice, parentPos)
+	path = append(path, parentPos)
 
 	for {
+
 		if childPos, exists := parent[parentPos]; exists {
-			resSlice = append([]position{childPos}, resSlice...)
+			path = append([]position{childPos}, path...)
 			parentPos = childPos
 		} else {
 			break
@@ -75,11 +78,82 @@ func reconstructPath(parentPos position, parent map[position]position) (resSlice
 	return
 }
 
+// Шахматные координаты в числовые
+func chessToNumeric(notation string) (position, error) {
+
+	if len(notation) != 2 {
+		return position{}, fmt.Errorf("неверные координаты: %s", notation)
+	}
+
+	lowerNotation := strings.ToLower(notation)
+	x := int(lowerNotation[0] - 'a')
+	y := int(lowerNotation[1] - '1')
+
+	res := position{x, y}
+
+	if !res.isValid() {
+		return position{}, fmt.Errorf("неверные координаты: %s", notation)
+	}
+
+	return res, nil
+}
+
+// Числовые координаты в шахматные
+func numericToChess(pos position) string {
+	return fmt.Sprintf("%c%d", 'a'+pos.x, pos.y+1)
+}
+
+// Вывод пути
+func formatPath(path []position) []string {
+	var result []string
+	for _, pos := range path {
+		result = append(result, numericToChess(pos))
+	}
+	return result
+}
+
 func main() {
 
-	startPos := position{2, 3}
-	endPos := position{6, 6}
+	var startPosString string
+	var endPosString string
+
+	fmt.Println("Введите начальные координаты:")
+	fmt.Scanln(&startPosString)
+	fmt.Println("Введите конечные координаты:")
+	fmt.Scanln(&endPosString)
+
+	startPos, err := chessToNumeric(startPosString)
+	if err != nil {
+		fmt.Printf("Ошибка! %v", err)
+		fmt.Scanln()
+		return
+	}
+
+	endPos, err := chessToNumeric(endPosString)
+	if err != nil {
+		fmt.Printf("Ошибка! %v", err)
+		fmt.Scanln()
+		return
+	}
 
 	shortestPath := findShortestPath(startPos, endPos)
-	fmt.Println(shortestPath)
+	fmt.Println("Количество шагов:", len(shortestPath)-1)
+	fmt.Println("Путь:", formatPath(shortestPath))
+
+	fmt.Scanln()
+
+	/* 	testCases := []testCases{
+		{position{3, 5}, position{6, 1}},
+		{position{0, 0}, position{7, 7}},
+		{position{0, 0}, position{2, 1}},
+		{position{0, 0}, position{0, 0}},
+	} */
+
+	/* 	for _, tc := range testCases {
+		shortestPath := findShortestPath(tc.start, tc.end)
+		fmt.Printf("Тест %d: %v -> %v\n", i+1, tc.start, tc.end)
+		fmt.Printf("Количество ходов %d: %v\n\n", len(shortestPath)-1, shortestPath)
+
+	} */
+
 }
